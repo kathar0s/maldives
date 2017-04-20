16cdd8646ed24b70ded6e4e634ea116da161d596
gyujin.choi <since630@gmail.com>
2017-04-20 18:19:27 +0900

    초기페이지 작성 local.py 데이터베이스 세팅

--- maldives/urls.py
+++ maldives/urls.py
@@ -18,8 +18,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from joonggo import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
