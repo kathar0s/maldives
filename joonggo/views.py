237282d52cc729da32a621964ea7391c7410292d
kathar0s <kathar0s@idsntory.com>
2017-06-01 20:25:48 +0900

    중고다이브 - 메인페이지 1차안 업로드

--- joonggo/views.py
+++ joonggo/views.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework.decorators import list_route
-from rest_framework.generics import GenericAPIView
-from rest_framework import serializers, mixins
 from rest_framework import viewsets
 from joonggo.models import Article, Alarm
 from joonggo.serializers import ArticleSerializer, AlarmSerializer
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Maldives!')
+    template_data = {}
+    return render(request, 'index.html', template_data)
+
 
 class PaginationClass(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'limit'
+
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
@@ -29,6 +29,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
         if item_id is not None:
             queryset = queryset.filter(title__contains=item_id)
         return queryset
+
 
 class AlarmViewSet(viewsets.ModelViewSet):
     queryset = Alarm.objects.all()
