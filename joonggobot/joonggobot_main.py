ef4b6321b0f63fbbd5afa5f4377ed190100c447c
neo <neo.chae.lg@gmail.com>
2017-06-06 13:34:22 +0900

    봇 검색 시 제목의 중복 공백 제거/시간의 뒷부분 제거

--- joonggobot/joonggobot_main.py
+++ joonggobot/joonggobot_main.py
@@ -139,8 +139,8 @@ class JoonggoBot:
             query_result = u"검색 결과 = %d 개\n\n" % (len(item_list))
             for index, row in item_list.iterrows():
                 query_result += u"가격 : %s\n" % (row['price'])
-                query_result += u"날짜 : %s\n" % (row['created'])
-                query_result += u"제목 : %s\n" % (row['title'])
+                query_result += u"날짜 : %s\n" % (row['created'].split(".")[0])
+                query_result += u"제목 : %s\n" % (' '.join(row['title'].split()))
 
                 default_url = u"%s\n\n" % (row['url'])
                 words = re.search(r"\[(.*)\]", row['source_id'])
