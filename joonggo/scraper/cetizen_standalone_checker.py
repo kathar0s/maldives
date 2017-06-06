d4d8d39af53eecb6214351300555d3059c487888
neo <neo.chae.lg@gmail.com>
2017-06-07 07:45:37 +0900

    cetizen 의 게시글에 대한 존재 여부 체크 1. 체크 조건, detail page내 제품 고유번호 일치 여부 확인 2. 체크 결과, Article table내 survival_count field 갱신    글이 존재할 경우 survival_count 1 부터 순차 증가    글이 존재하지 않을 경우 0으로 설정

--- joonggo/scraper/cetizen_standalone_checker.py
+++ joonggo/scraper/cetizen_standalone_checker.py
@@ -21,21 +21,26 @@ class CetizenStandaloneChecker(DjangoSpider):
         self.scheduler_runtime = self.ref_object.scraper_runtime
         self.scraped_obj_class = Article
         self.scraped_obj_item_class = ArticleItem
+        self.check_article_ids = Article.objects.filter(
+            Q(source=self.ref_object) & Q(survival_count=1)).order_by("id").values_list("id", flat=True)
+        self.check_current_index = 0
 
         super(CetizenStandaloneChecker, self).__init__(self, *args, **kwargs)
 
+    def request_next_url(self):
+        #article = Article.objects.get(id=self.check_article_ids[self.check_current_index])
+        articles = Article.objects.filter(Q(source=self.ref_object) & Q(survival_count=1)).order_by("id")[:10]
+
+        article = articles[0]
+        print(u"checking : %d %s %s %s\n" % (article.id, article.title, article.uid, article.url))
+        self.check_current_index += 1
+        return Request(article.url, callback=self.detail_parse, meta={'article': article})
+
+
     def start_requests(self):
-        print(u'CetizenStandaloneChecker start')
-
-        index = 0
-        articles = Article.objects.filter(Q(source=self.ref_object) & Q(survival_count__gte=1)).order_by('id')[:10]
-        print(u"checking (%d) articles\n" % (len(articles)))
-        for article in articles:
-            print(u"checking : %d %s\n" % (article.id, article.url))
-            yield Request(article.url, callback=self.detail_parse, meta={'article': article})
-            time.sleep(1)
-
-        print(u'CetizenStandaloneChecker ends')
+        print(u'start_requests start')
+        yield self.request_next_url()
+        print(u'start_requests ends')
 
 
     def detail_parse(self, response):
@@ -44,10 +49,16 @@ class CetizenStandaloneChecker(DjangoSpider):
 
         article = response.meta['article']
         print(u"title : %s, uid : %s" % (article.title, article.uid))
+        print(u"response url : %s," % (response.url))
         if uid is None or uid not in article.uid:
             print(u"%s, %s mismatch\n%s" % (article.uid, uid, article.url))
+            article.survival_count = 0
         else:
             print(u"%s, %s match\n%s" % (article.uid, uid, article.url))
+            article.survival_count += 1
+        article.save()
+
+        yield self.request_next_url()
 
 
 
