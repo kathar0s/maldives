0ebebeaf40bb1a070755148a83e29fe431a407c0
kathar0s <kathar0s@idsntory.com>
2017-06-07 21:01:11 +0900

    가격 검색 - 중고가격 추이 그래프 추가

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ import json
 import datetime
 
 from django.contrib.auth import authenticate, login as django_login
-from django.db.models import Avg, Min, Max
+from django.db.models import Avg, Min, Max, Q
 from django.http import JsonResponse
 from django_pandas.io import read_frame
 from rest_framework import viewsets, filters
@@ -29,15 +29,6 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
     info = {}
 
-    # def get_paginated_response(self, data):
-    #     return Response(OrderedDict([
-    #         ('count', self.page.paginator.count),
-    #         ('next', self.get_next_link()),
-    #         ('previous', self.get_previous_link()),
-    #         ('info', self.info),
-    #         ('results', data)
-    #     ]))
-
     # 키워드로 검색하는 경우에 대해서 처리한다.
     @list_route()
     def search(self, request):
@@ -54,20 +45,20 @@ class ArticleViewSet(viewsets.ModelViewSet):
                                        title__icontains=query).order_by('price')
 
             # 검색이 된 경우에만 평균가격을 산출하고 진행한다.
-            #if queryset.count() > 0:
-            result = queryset.aggregate(avg_price=Avg('price'))
-            avg_price = result['avg_price']
+            if queryset.count() > 0:
+                result = queryset.aggregate(avg_price=Avg('price'))
+                avg_price = result['avg_price']
 
-            # 검색결과의 노이즈가 있을 수 있으니
-            # 최저가격은 평균가격의 20%보다 작은 것들은 제외하고
-            # 최고가격은 평균가격의 3배보다 큰 것들은 제외한다.
-            queryset = queryset.filter(price__gte=avg_price * 0.2, price__lt=avg_price * 3)
+                # 검색결과의 노이즈가 있을 수 있으니
+                # 최저가격은 평균가격의 20%보다 작은 것들은 제외하고
+                # 최고가격은 평균가격의 3배보다 큰 것들은 제외한다.
+                queryset = queryset.filter(price__gte=avg_price * 0.2, price__lt=avg_price * 3)
 
-            # 해당 검색결과에서 최대, 최소 가격을 구한다.
-            result = queryset.aggregate(max_price=Max('price'), min_price=Min('price'))
-            self.info = result
-            #else:
-            #    info = None
+                # 해당 검색결과에서 최대, 최소 가격을 구한다.
+                result = queryset.aggregate(max_price=Max('price'), min_price=Min('price'))
+                self.info = result
+            else:
+                info = None
 
             page = self.paginate_queryset(queryset)
 
@@ -93,6 +84,13 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
         return Response(template_data)
 
+    @list_route()
+    def trend(self, request):
+        get = request.GET.copy()
+        if 'q' in get and get['q'] != '':
+            query = get['q']
+            return Response(retrieve_item(query))
+
 
 class AlarmViewSet(viewsets.ModelViewSet):
     queryset = Alarm.objects.all().select_related('profile').order_by('id')
@@ -102,18 +100,21 @@ class AlarmViewSet(viewsets.ModelViewSet):
     filter_fields = ('profile', 'id', 'profile__chat', 'profile__user')
 
 
-def retrieve_item(item_id):
+def retrieve_item(keyword):
     # 최근 2주일 데이터만 조회
     end_date = datetime.date.today()  # 현재 날짜 가져오기
     period = datetime.timedelta(days=13)
     start_date = end_date - period
     queryset = Article.objects.filter(created__gte=start_date).order_by('-created')
-    queryset = queryset.filter(title__contains=item_id)
+    # queryset = Article.objects.all()
+    queryset = queryset.filter(is_sold_out=False, survival_count__gte=1, price__gte=10000, title__icontains=keyword)
 
     # 판매 글만 조회 도도록 문구 제거
     title_exclude = ['삽니다', '구합니다', '배터리']
-    for t in title_exclude:
-        queryset = queryset.exclude(title__contains=t)
+    exclude_query = Q(title__icontains=title_exclude[0])
+    for t in title_exclude[1:]:
+        exclude_query = exclude_query | Q(title__icontains=t)
+    queryset = queryset.exclude(exclude_query)
 
     if queryset.count() > 0:
         df_article_data = read_frame(queryset,
@@ -123,7 +124,7 @@ def retrieve_item(item_id):
 
         # 날짜 형식 변환 및 trend date 생성
         df_article_data['trend_date'] = df_article_data['created'].apply(lambda x: x.strftime('%m-%d'))
-        df_article_data['created'] = df_article_data['created'].apply(lambda x: x.strftime('%Y-%m-%d %hh:%mm:%ss'))
+        df_article_data['created'] = df_article_data['created'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
 
         # 평균값의 20%의 가격으로 최저가 책정/ 평균값의 3배 가격으로 최고가 책정
         avg_price = df_article_data['price'].mean()
@@ -141,10 +142,10 @@ def retrieve_item(item_id):
         min_daily = df_article_data.groupby(['trend_date'])['price'].min()
 
         # dictionary 변경
-        df_article_data = df_article_data.T
-        dict_article_data = df_article_data.to_dict()
-        dict_article_data = collections.OrderedDict(sorted(dict_article_data.items()))
-        dict_article_data = check_key_type(dict_article_data)
+        # df_article_data = df_article_data.T
+        # dict_article_data = df_article_data.to_dict()
+        # dict_article_data = collections.OrderedDict(sorted(dict_article_data.items()))
+        # dict_article_data = check_key_type(dict_article_data)
 
         dict_avg_daily = avg_daily.to_dict()
         dict_avg_daily = collections.OrderedDict(sorted(dict_avg_daily.items()))
@@ -161,17 +162,18 @@ def retrieve_item(item_id):
         for key in dict_min_daily.keys():
             list_min_price.append(dict_min_daily[key])
 
-        content = {'rsltCd': 'Y',
-                   'rsltNsg': '정상',
-                   'article_data': dict_article_data,       # 중고 물건 목록
-                   'trend_day': json.dumps(list_avg_date),  # 평균가/최저가 날짜 - List
-                   'trend_avg_price': list_avg_price,       # 평균가 추세 - List
-                   'trend_min_price': list_min_price,       # 최저가 추세 - List
-                   'min_price': min_price,                  # 최저가
-                   'max_price': max_price                   # 최고가
-                   }
+        content = {
+            'rsltCd': 'Y',
+            'rsltNsg': u'정상',
+            # 'article_data': dict_article_data,    # 중고 물건 목록
+            'trend_day': list_avg_date,             # 평균가/최저가 날짜 - List
+            'trend_avg_price': list_avg_price,      # 평균가 추세 - List
+            'trend_min_price': list_min_price,      # 최저가 추세 - List
+            'min_price': min_price,                 # 최저가
+            'max_price': max_price                  # 최고가
+        }
     else:
-        content = {'rsltCd': 'N', 'rsltNsg': '데이터가 없음'}
+        content = {'rsltCd': 'N', 'rsltNsg': u'데이터가 없음'}
 
     return content
 
@@ -188,7 +190,7 @@ def check_key_type(dict):
                 except:
                     pass
             del dict[key]
-    return  dict
+    return dict
 
 
 def login(request):
